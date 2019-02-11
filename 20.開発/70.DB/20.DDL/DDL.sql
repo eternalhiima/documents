ba03c2e3db987235a01d90863d6fc4d625c8efc5
taiki0304-vaio <f1x.mi1kt2748@gmail.com>
2019-02-11 16:47:28 +0900

    [modify] ER図

--- 20.開発/70.DB/20.DDL/DDL.sql
+++ 20.開発/70.DB/20.DDL/DDL.sql
@@ -1,171 +1,107 @@
 -- Project Name : hiima
--- Date/Time    : 2019/01/30 22:53:30
--- Author       : simplex
+-- Date/Time    : 2019/02/11 15:24:38
+-- Author       : rebor
 -- RDBMS Type   : MySQL
 -- Application  : A5:SQL Mk-2
 
-/*
-  BackupToTempTable, RestoreFromTempTable疑似命令が付加されています。
-  これにより、drop table, create table 後もデータが残ります。
-  この機能は一時的に $$TableName のような一時テーブルを作成します。
-*/
+-- カテゴリ
+drop table `CATEGORY` cascade;
 
--- タグ
---* BackupToTempTable
-drop table if exists `TAG` cascade;
-
---* RestoreFromTempTable
-create table `TAG` (
-  `TAG_ID` DECIMAL(8) not null comment 'タグID'
-  , `TAG_NAME` VARCHAR(12) not null comment 'タグ名'
-  , `USER_ID` DECIMAL(8) not null comment 'ユーザーID'
+create table `CATEGORY` (
+  `CATEGORY_ID` DECIMAL(13,0) not null comment 'カテゴリID'
+  , `CATEGORY_NAME` VARCHAR(12) not null comment 'カテゴリ名'
+  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
   , `CREATE_DATETIME` DATETIME not null comment '作成日時'
-  , `DELETE_DATETIME` DATETIME comment '削除日時'
-  , `USED_COUNT` DECIMAL(8) not null comment '使用回数'
+  , `USED_COUNT` DECIMAL(13,0) not null comment '使用回数'
   , `LAST_USED_DATETIME` DATETIME not null comment '最終使用日時'
-  , `IS_DELETED` INT not null comment '削除フラグ'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
-) comment 'タグ' ;
-
-create unique index `TAG_PKI`
-  on `TAG`(`TAG_ID`);
-
-alter table `TAG`
-  add constraint `TAG_PKC` primary key (`TAG_ID`);
+  , constraint `CATEGORY_PKC` primary key (`CATEGORY_ID`)
+) comment 'カテゴリ' ;
 
 -- トーク評価
---* BackupToTempTable
-drop table if exists `TALK_EVALUATION` cascade;
+drop table `TALK_EVALUATION` cascade;
 
---* RestoreFromTempTable
 create table `TALK_EVALUATION` (
-  `TALK_HISTORY_ID` DECIMAL(8) not null comment 'トーク評価ID'
-  , `USER_ID` VARCHAR(8) comment 'ユーザーID'
-  , `TALK_THEME_ID` VARCHAR(8) not null comment 'トークテーマID'
-  , `EVALUATION` INT not null comment '評価'
-  , `IS_DELETED` INT not null comment '削除フラグ'
+  `TALK_EVALUATION_ID` DECIMAL(13,0) not null comment 'トーク評価ID'
+  , `USER_ID` DECIMAL(13,0) comment 'ユーザーID'
+  , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
+  , `EVALUATION` DECIMAL(2,0) not null comment '評価'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
+  , constraint `TALK_EVALUATION_PKC` primary key (`TALK_EVALUATION_ID`)
 ) comment 'トーク評価' ;
 
-create unique index `TALK_EVALUATION_PKI`
-  on `TALK_EVALUATION`(`TALK_HISTORY_ID`);
-
-alter table `TALK_EVALUATION`
-  add constraint `TALK_EVALUATION_PKC` primary key (`TALK_HISTORY_ID`);
-
 -- トーク履歴
---* BackupToTempTable
-drop table if exists `TALK_HISTORY` cascade;
+drop table `TALK_HISTORY` cascade;
 
---* RestoreFromTempTable
 create table `TALK_HISTORY` (
-  `TALK_HISTORY_ID` DECIMAL(8) not null comment 'トーク履歴ID'
-  , `USER_ID` VARCHAR(8) comment 'ユーザーID'
-  , `TALK_THEME_ID` VARCHAR(8) not null comment 'トークテーマID'
+  `TALK_HISTORY_ID` DECIMAL(13,0) not null comment 'トーク履歴ID'
+  , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
+  , `USER_ID` DECIMAL(13,0) comment 'ユーザーID'
   , `START_DATETIME` DATETIME not null comment '開始時刻'
-  , `END_DATETIME` DATETIME comment '終了時刻'
-  , `IS_DELETED` INT not null comment '削除フラグ'
+  , `END_DATETIME` DATETIME not null comment '終了時刻'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
+  , constraint `TALK_HISTORY_PKC` primary key (`TALK_HISTORY_ID`)
 ) comment 'トーク履歴' ;
 
-create unique index `TALK_HISTORY_PKI`
-  on `TALK_HISTORY`(`TALK_HISTORY_ID`);
+-- トークテーマカテゴリバインド
+drop table `TALK_THEME_CATEGORY_BIND` cascade;
 
-alter table `TALK_HISTORY`
-  add constraint `TALK_HISTORY_PKC` primary key (`TALK_HISTORY_ID`);
-
--- トークテーマタグバインド
---* BackupToTempTable
-drop table if exists `TALK_THEME_TAG_BIND` cascade;
-
---* RestoreFromTempTable
-create table `TALK_THEME_TAG_BIND` (
-  `TALK_THEME_TAG_BIND_ID` DECIMAL(8) not null comment 'トークテーマタグバインドID'
-  , `TAG_ID` DECIMAL(8) not null comment 'タグID'
-  , `TALK_THEME_ID` DECIMAL(8) not null comment 'トークテーマID'
-  , `USER_ID` DECIMAL(8) not null comment 'ユーザーID'
-  , `TAG_DATETIME` DATETIME not null comment 'タグ付け日時'
-  , `IS_DELETED` INT not null comment '削除フラグ'
+create table `TALK_THEME_CATEGORY_BIND` (
+  `TALK_THEME_CATEGORY_BIND_ID` DECIMAL(13,0) not null comment 'トークテーマカテゴリバインドID'
+  , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
+  , `CATEGORY_ID` DECIMAL(13,0) not null comment 'カテゴリID'
+  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
-) comment 'トークテーマタグバインド' ;
-
-create unique index `TALK_THEME_TAG_BIND_PKI`
-  on `TALK_THEME_TAG_BIND`(`TALK_THEME_TAG_BIND_ID`);
-
-alter table `TALK_THEME_TAG_BIND`
-  add constraint `TALK_THEME_TAG_BIND_PKC` primary key (`TALK_THEME_TAG_BIND_ID`);
+  , constraint `TALK_THEME_CATEGORY_BIND_PKC` primary key (`TALK_THEME_CATEGORY_BIND_ID`)
+) comment 'トークテーマカテゴリバインド' ;
 
 -- コメント
---* BackupToTempTable
-drop table if exists `COMMENT` cascade;
+drop table `COMMENT` cascade;
 
---* RestoreFromTempTable
 create table `COMMENT` (
-  `COMMENT_ID` DECIMAL(8) not null comment 'コメントID'
+  `COMMENT_ID` DECIMAL(13,0) not null comment 'コメントID'
+  , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
   , `COMMENT` VARCHAR(50) not null comment 'コメント'
-  , `USER_ID` DECIMAL(8) not null comment 'ユーザーID'
-  , `TALK_THEME_ID` DECIMAL(8) not null comment 'トークテーマID'
+  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
   , `CREATE_DATETIME` DATETIME not null comment '作成日時'
-  , `DELETE_DATETIME` DATETIME comment '削除日時'
-  , `IS_DELETED` INT not null comment '削除フラグ'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
+  , constraint `COMMENT_PKC` primary key (`COMMENT_ID`)
 ) comment 'コメント' ;
 
-create unique index `COMMENT_PKI`
-  on `COMMENT`(`COMMENT_ID`);
-
-alter table `COMMENT`
-  add constraint `COMMENT_PKC` primary key (`COMMENT_ID`);
-
 -- トークテーマ
---* BackupToTempTable
-drop table if exists `TALK_THEME` cascade;
+drop table `TALK_THEME` cascade;
 
---* RestoreFromTempTable
 create table `TALK_THEME` (
-  `TALK_THEME_ID` DECIMAL(8) not null comment 'トークテーマID'
+  `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
   , `TITLE` VARCHAR(30) not null comment 'タイトル'
   , `CONTENT` VARCHAR(500) not null comment '内容'
-  , `THUMBNAIL_PATH` VARCHAR(50) not null comment 'サムネイルパス'
-  , `RELATED_URL` VARCHAR(100) comment '関連URL'
-  , `USER_ID` DECIMAL(8) not null comment 'ユーザーID'
+  , `THUMBNAIL_PATH` VARCHAR(2100) comment 'サムネイルパス'
+  , `RELATED_URL` VARCHAR(2100) comment '関連URL'
+  , `GOOD_COUNT` DECIMAL(13,0) default 0 not null comment '高評価数'
+  , `BAD_COUNT` DECIMAL(13,0) default 0 not null comment '低評価数'
+  , `TALK_COUNT` DECIMAL(13,0) default 0 not null comment 'トーク数'
+  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
   , `CREATE_DATETIME` DATETIME not null comment '作成日時'
-  , `DELETE_DATETIME` DATETIME comment '削除日時'
-  , `IS_DELETED` INT not null comment '削除フラグ'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
+  , constraint `TALK_THEME_PKC` primary key (`TALK_THEME_ID`)
 ) comment 'トークテーマ' ;
 
-create unique index `TALK_THEME_PKI`
-  on `TALK_THEME`(`TALK_THEME_ID`);
-
-alter table `TALK_THEME`
-  add constraint `TALK_THEME_PKC` primary key (`TALK_THEME_ID`);
-
 -- ユーザー
---* BackupToTempTable
-drop table if exists `USER` cascade;
+drop table `USER` cascade;
 
---* RestoreFromTempTable
 create table `USER` (
-  `USER_ID` DECIMAL(8) not null comment 'ユーザーID'
+  `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
   , `USER_NAME` VARCHAR(10) not null comment 'ユーザー名'
-  , `ICON_PATH` VARCHAR(100) not null comment 'アイコンパス'
+  , `ICON_PATH` VARCHAR(100) comment 'アイコンパス'
   , `CREATE_DATETIME` DATETIME not null comment '作成日時'
-  , `DELETE_DATETIME` DATETIME comment '削除日時'
-  , `IS_DELETED` INT not null comment '削除フラグ'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
+  , constraint `USER_PKC` primary key (`USER_ID`)
 ) comment 'ユーザー' ;
-
-create unique index `USER_PKI`
-  on `USER`(`USER_ID`);
-
-alter table `USER`
-  add constraint `USER_PKC` primary key (`USER_ID`);
 
