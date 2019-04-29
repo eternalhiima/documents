080c9a059a9de246c679c2636750e2dea349e351
taiki0304-vaio <f1x.mi1kt2748@gmail.com>
2019-04-29 23:58:35 +0900

    [modify] ERDiaram API仕様書

--- 20.開発/70.DB/20.DDL/DDL.sql
+++ 20.開発/70.DB/20.DDL/DDL.sql
@@ -1,5 +1,5 @@
 -- Project Name : hiima
--- Date/Time    : 2019/02/20 22:42:46
+-- Date/Time    : 2019/04/29 23:53:55
 -- Author       : rebor
 -- RDBMS Type   : MySQL
 -- Application  : A5:SQL Mk-2
@@ -10,7 +10,6 @@ drop table `CATEGORY` cascade;
 create table `CATEGORY` (
   `ID` BIGINT not null auto_increment comment 'カテゴリID'
   , `CATEGORY_NAME` VARCHAR(12) not null comment 'カテゴリ名'
-  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
   , `CREATE_DATETIME` DATETIME not null comment '作成日時'
   , `USED_COUNT` DECIMAL(13,0) not null comment '使用回数'
   , `LAST_USED_DATETIME` DATETIME not null comment '最終使用日時'
@@ -24,7 +23,7 @@ drop table `TALK_EVALUATION` cascade;
 
 create table `TALK_EVALUATION` (
   `ID` BIGINT not null auto_increment comment 'トーク評価ID'
-  , `USER_ID` DECIMAL(13,0) comment 'ユーザーID'
+  , `USER_ID` BIGINT comment 'ユーザーID'
   , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
   , `EVALUATION` DECIMAL(2,0) not null comment '評価'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
@@ -37,7 +36,7 @@ drop table `TALK_HISTORY` cascade;
 
 create table `TALK_HISTORY` (
   `ID` BIGINT not null auto_increment comment 'トーク履歴ID'
-  , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
+  , `TALK_THEME_ID` BIGINT not null comment 'トークテーマID'
   , `USER_ID` DECIMAL(13,0) comment 'ユーザーID'
   , `START_DATETIME` DATETIME not null comment '開始時刻'
   , `END_DATETIME` DATETIME not null comment '終了時刻'
@@ -50,13 +49,10 @@ create table `TALK_HISTORY` (
 drop table `TALK_THEME_CATEGORY_BIND` cascade;
 
 create table `TALK_THEME_CATEGORY_BIND` (
-  `ID` BIGINT not null auto_increment comment 'トークテーマカテゴリバインドID'
-  , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
-  , `CATEGORY_ID` DECIMAL(13,0) not null comment 'カテゴリID'
-  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
+  `TALK_THEME_ID` BIGINT not null comment 'トークテーマID'
+  , `CATEGORY_ID` BIGINT not null comment 'カテゴリID'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
-  , constraint `TALK_THEME_CATEGORY_BIND_PKC` primary key (`ID`)
 ) comment 'トークテーマカテゴリバインド' ;
 
 -- コメント
@@ -64,9 +60,9 @@ drop table `COMMENT` cascade;
 
 create table `COMMENT` (
   `ID` BIGINT not null auto_increment comment 'コメントID'
-  , `TALK_THEME_ID` DECIMAL(13,0) not null comment 'トークテーマID'
+  , `TALK_THEME_ID` BIGINT not null comment 'トークテーマID'
   , `COMMENT` VARCHAR(50) not null comment 'コメント'
-  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
+  , `USER_ID` BIGINT not null comment 'ユーザーID'
   , `CREATE_DATETIME` DATETIME not null comment '作成日時'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
@@ -82,10 +78,10 @@ create table `TALK_THEME` (
   , `CONTENT` VARCHAR(500) not null comment '内容'
   , `THUMBNAIL_PATH` VARCHAR(2100) comment 'サムネイルパス'
   , `RELATED_URL` VARCHAR(2100) comment '関連URL'
-  , `GOOD_COUNT` DECIMAL(13,0) default 0 not null comment '高評価数'
-  , `BAD_COUNT` DECIMAL(13,0) default 0 not null comment '低評価数'
-  , `TALK_COUNT` DECIMAL(13,0) default 0 not null comment 'トーク数'
-  , `USER_ID` DECIMAL(13,0) not null comment 'ユーザーID'
+  , `GOOD_COUNT` DECIMAL(13,0) default 0 comment '高評価数'
+  , `BAD_COUNT` DECIMAL(13,0) default 0 comment '低評価数'
+  , `TALK_COUNT` DECIMAL(13,0) default 0 comment 'トーク数'
+  , `USER_ID` BIGINT not null comment 'ユーザーID'
   , `CREATE_DATETIME` DATETIME not null comment '作成日時'
   , `INPUT_DATETIME` DATETIME not null comment '入力日時'
   , `UPDATE_DATETIME` DATETIME not null comment '更新日時'
